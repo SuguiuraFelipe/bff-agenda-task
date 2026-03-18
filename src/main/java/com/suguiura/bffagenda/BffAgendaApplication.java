123f36779a65acbbf058e907c6f00a5af2c5a06a
SuguiuraFelipe <felipesuguiura.pro@gmail.com>
2026-03-18 10:31:56 +0000

    Skeleton added to the project.

---
+++ src/main/java/com/suguiura/bffagenda/BffAgendaApplication.java
@@ -0,0 +1,13 @@
+package com.suguiura.bffagenda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BffAgendaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BffAgendaApplication.class, args);
+	}
+
+}
