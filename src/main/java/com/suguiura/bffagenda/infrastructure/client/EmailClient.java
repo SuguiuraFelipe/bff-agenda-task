87e8ac1e9bfcbfd1c228b72b03ade3ed745222bf
SuguiuraFelipe <felipesuguiura.pro@gmail.com>
2026-03-19 20:40:21 +0000

    add client email

---
+++ src/main/java/com/suguiura/bffagenda/infrastructure/client/EmailClient.java
@@ -0,0 +1,14 @@
+package com.suguiura.bffagenda.infrastructure.client;
+
+import com.suguiura.bffagenda.business.dto.out.TaskDTOResponse;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@FeignClient(name = "notification", url = "${notification.url}")
+public interface EmailClient {
+
+    void sendEmail(@RequestBody TaskDTOResponse dto);
+}
